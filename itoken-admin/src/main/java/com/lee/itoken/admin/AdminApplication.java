cbce592ae700a285f9e909e56cf2fe2f810a41cc
wangle <251668577@qq.com>
2019-07-19 09:54:38 +0800

    初始化提交

---
+++ itoken-admin/src/main/java/com/lee/itoken/admin/AdminApplication.java
@@ -0,0 +1,20 @@
+package com.lee.itoken.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author WangLe
+ * @date 2019/7/19 9:52
+ * @description
+ */
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
