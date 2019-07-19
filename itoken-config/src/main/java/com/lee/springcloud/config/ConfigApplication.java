d57c56df519143f2d5f6e271272cd198205773ef
wangle <251668577@qq.com>
2019-07-19 11:29:44 +0800

    配置中心测试

--- itoken-config/src/main/java/com/lee/springcloud/config/ConfigApplication.java
+++ itoken-config/src/main/java/com/lee/springcloud/config/ConfigApplication.java
@@ -2,6 +2,7 @@ package com.lee.springcloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
