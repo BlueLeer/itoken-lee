e58c11c318d4bd709c0a936755963bce94f5ead9
wangle <251668577@qq.com>
2019-07-19 10:26:23 +0800

    配置中心测试

---
+++ itoken-server/src/main/java/com/lee/springcloud/server/ServerApplication.java
@@ -0,0 +1,18 @@
+package com.lee.springcloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author WangLe
+ * @date 2019/7/19 10:25
+ * @description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
