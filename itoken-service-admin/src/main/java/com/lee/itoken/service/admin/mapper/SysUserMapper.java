c35f33440515f3f543d2ccf4e8f1df78f018007c
wangle <251668577@qq.com>
2019-07-23 14:50:20 +0800

    配置阿里云环境

--- itoken-service-admin/src/main/java/com/lee/itoken/service/admin/mapper/SysUserMapper.java
+++ itoken-service-admin/src/main/java/com/lee/itoken/service/admin/mapper/SysUserMapper.java
@@ -2,6 +2,7 @@ package com.lee.itoken.service.admin.mapper;
 
 import com.lee.itoken.service.admin.entity.SysUser;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author WangLe
  * @since 2019-07-23
  */
+@Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> {
 
 }
